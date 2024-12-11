9bee6202246a44cba4e63c497be7e12116d64d1c
dUbster-98 <tjdgus981109@naver.com>
2024-12-11 12:03:21 +0900

    Initial commit

---
+++ DatePicker/DatePicker/App.cs
@@ -0,0 +1,21 @@
+﻿using DatePicker.UI.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DatePicker
+{
+    internal class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow window = new();
+            window.ShowDialog();
+        }
+    }
+}
